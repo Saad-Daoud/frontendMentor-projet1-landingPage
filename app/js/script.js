863063b9a54512d3e8900b34b2be595cbcaf374b
Saad-Daoud <h.saad.daoud@gmail.com>
2021-10-20 11:27:18 +0100

    header finished

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,9 @@
 const btnHamb= document.querySelector('#btnHamb');
-const header = document.querySelector('.header')
+const header = document.querySelector('.header');
+const body = document.querySelector('body');
 
-btnHamb.addEventListener('click', (e) => (
-    
+btnHamb.addEventListener('click', function (e) {
     header.classList.toggle('open')
-    
-    ))  
+    body.classList.toggle('hiddenScroll')
+})
+
